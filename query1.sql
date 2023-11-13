328167cde06118d16656ea26c4f816e72eecbb75
Pykroonin <manuelferlo15@gmail.com>
2023-11-13 07:12:28 +0100

    queries optimizadas y puestas bonitas

--- query1.sql
+++ query1.sql
@@ -1,88 +1,116 @@
-WITH asientos_totales
-     AS (SELECT f.flight_id,
-                COUNT(s.seat_no) AS as_totales
-         FROM   seats s,
-                aircrafts_data ad,
-                flights f
-         WHERE  s.aircraft_code = ad.aircraft_code
-                AND ad.aircraft_code = f.aircraft_code
-         GROUP  BY f.flight_id),
-     asientos_ocupados
-     AS (SELECT f.flight_id,
-                COUNT(tf.ticket_no) AS as_ocupados
-         FROM   flights f,
-                ticket_flights tf
-         WHERE  f.flight_id = tf.flight_id
-         GROUP  BY f.flight_id),
-     asientos_libres
-     AS (SELECT t.flight_id,
-                ( t.as_totales - Coalesce(o.as_ocupados, 0) ) AS vacios
-         FROM   asientos_totales t
-                LEFT JOIN asientos_ocupados o
-                       ON t.flight_id = o.flight_id),
-     vuelos_directos
-     AS (SELECT f.flight_id,
-               f.scheduled_departure ,
-               f.scheduled_arrival,
-f.flight_id,
-               f.scheduled_departure ,
-               f.scheduled_arrival,
-               0                                                 AS transbordos,
-               al.vacios                                   AS asientos,
-               ( f.scheduled_arrival  - f.scheduled_departure  ) AS tiempo,
-               f.aircraft_code
-         FROM   flights f
-                JOIN asientos_libres al
-                  ON f.flight_id = al.flight_id
-         WHERE  f.departure_airport = 'SVO'
-                AND f.arrival_airport = 'VKO'
-                AND DATE(f.scheduled_departure) = '2017-09-12'
-                AND al.vacios > '0'),
-     vuelos_salida
-     AS (SELECT f.flight_id,
-                f.scheduled_departure as fecha_sal,
-                f.scheduled_arrival as fecha_lleg2,
+WITH vuelos_salida
+AS
+  (
+         SELECT f.flight_id,
+                f.scheduled_departure AS fecha_sal,
+                f.scheduled_arrival   AS fecha_lleg2,
                 f.arrival_airport,
                 f.aircraft_code
          FROM   flights f
          WHERE  f.departure_airport = 'SVO'
-                AND f.scheduled_departure >= '2017-09-12'
-                AND scheduled_departure < '2017-09-12'::DATE+1),
-     vuelos_llegada
-     AS (SELECT f.flight_id as flight_id2,
-                f.scheduled_departure as fecha_sal2,
-                f.scheduled_arrival as fecha_lleg,
-                f.departure_airport as departure_airport2,
-                f.aircraft_code as aircraft_code2
+         AND    f.scheduled_departure >= '2017-09-12'
+         AND    scheduled_departure < '2017-09-12'::date+1),
+  vuelos_llegada
+AS
+  (
+         SELECT f.flight_id           AS flight_id2,
+                f.scheduled_departure AS fecha_sal2,
+                f.scheduled_arrival   AS fecha_lleg,
+                f.departure_airport   AS departure_airport2,
+                f.aircraft_code       AS aircraft_code2
          FROM   flights f
          WHERE  f.arrival_airport = 'VKO'
-                AND f.scheduled_departure > '2017-09-12'),
-     vuelos_transbordo
-     AS (SELECT vs.flight_id,
-               vs.fecha_sal,
-               vs.fecha_lleg2,
-               vl.flight_id2,
-               vl.fecha_sal2,
-               vl.fecha_lleg,
-               1                                                 AS transbordos,
-               Min(al.vacios)                                    AS asientos,
-               ( vl.fecha_lleg - vs.fecha_sal ) AS tiempo,
-               vs.aircraft_code
-        FROM   vuelos_salida vs,
-               vuelos_llegada vl,
-               asientos_libres al
-        WHERE  vs.arrival_airport = vl.departure_airport2
-               AND vs.fecha_lleg2 < vl.fecha_sal2
-               AND vl.fecha_lleg - vs.fecha_sal <
-                   '24:00:00.000'
-               AND ( vs.flight_id = al.flight_id
-                      OR vl.flight_id2 = al.flight_id )
-        GROUP  BY vs.flight_id,
-                  vs.fecha_sal,
-                  vs.fecha_lleg2,
-                  vl.flight_id2,
-                  vl.fecha_sal2,
-                  vl.fecha_lleg,
-                  vs.aircraft_code)
- select * from (select * from vuelos_transbordo union ALL select * from vuelos_directos) order by (fecha_sal - fecha_lleg)
-
+         AND    f.scheduled_departure > '2017-09-12'),
+  asientos_totales
+AS
+  (
+           SELECT   vs.flight_id,
+                    count(s.seat_no) AS as_totales
+           FROM     seats s,
+                    vuelos_salida vs
+           WHERE    s.aircraft_code = vs.aircraft_code
+           GROUP BY vs.flight_id
+           UNION
+                 DISTINCT
+           SELECT   vl.flight_id2,
+                    count(s.seat_no) AS as_totales
+           FROM     seats s,
+                    vuelos_llegada vl
+           WHERE    s.aircraft_code = vl.aircraft_code2
+           GROUP BY vl.flight_id2),
+  asientos_ocupados
+AS
+  (
+           SELECT   vs.flight_id,
+                    count(tf.ticket_no) AS as_ocupados
+           FROM     vuelos_salida vs,
+                    ticket_flights tf
+           WHERE    vs.flight_id = tf.flight_id
+           GROUP BY vs.flight_id
+           UNION
+                 DISTINCT
+           SELECT   vl.flight_id2,
+                    count(tf.ticket_no) AS as_ocupados
+           FROM     vuelos_llegada vl,
+                    ticket_flights tf
+           WHERE    vl.flight_id2 = tf.flight_id
+           GROUP BY vl.flight_id2),
+  asientos_libres
+AS
+  (
+            SELECT    t.flight_id,
+                      ( t.as_totales - coalesce(o.as_ocupados, 0) ) AS vacios
+            FROM      asientos_totales t
+            LEFT JOIN asientos_ocupados o
+            ON        t.flight_id = o.flight_id)
+  SELECT   *
+  FROM     (
+                  SELECT *
+                  FROM   (
+                                  SELECT   vs.flight_id,
+                                           vs.fecha_sal,
+                                           vs.fecha_lleg2,
+                                           vl.flight_id2,
+                                           vl.fecha_sal2,
+                                           vl.fecha_lleg,
+                                           '1'                              AS transbordos,
+                                           min(al.vacios)                   AS asientos,
+                                           ( vl.fecha_lleg - vs.fecha_sal ) AS tiempo,
+                                           vs.aircraft_code
+                                  FROM     vuelos_salida vs,
+                                           vuelos_llegada vl,
+                                           asientos_libres al
+                                  WHERE    vs.arrival_airport = vl.departure_airport2
+                                  AND      vs.fecha_lleg2 < vl.fecha_sal2
+                                  AND      vl.fecha_lleg - vs.fecha_sal < '24:00:00.000'
+                                  AND      (
+                                                    vs.flight_id = al.flight_id
+                                           OR       vl.flight_id2 = al.flight_id )
+                                  GROUP BY vs.flight_id,
+                                           vs.fecha_sal,
+                                           vs.fecha_lleg2,
+                                           vl.flight_id2,
+                                           vl.fecha_sal2,
+                                           vl.fecha_lleg,
+                                           vs.aircraft_code)
+                UNION ALL
+                SELECT *
+                FROM   (
+                              SELECT f.flight_id,
+                                     f.scheduled_departure ,
+                                     f.scheduled_arrival,
+                                     f.flight_id,
+                                     f.scheduled_departure ,
+                                     f.scheduled_arrival,
+                                     '0'                                             AS transbordos,
+                                     al.vacios                                       AS asientos,
+                                     ( f.scheduled_arrival - f.scheduled_departure ) AS tiempo,
+                                     f.aircraft_code
+                              FROM   flights f
+                              JOIN   asientos_libres al
+                              ON     f.flight_id = al.flight_id
+                              WHERE  f.departure_airport = 'SVO'
+                              AND    f.arrival_airport = 'VKO'
+                              AND    date(f.scheduled_departure) = '2017-09-12'
+                              AND    al.vacios > '0'))
+  ORDER BY tiempo
--- query2.sql
+++ query2.sql
@@ -1,39 +1,74 @@
-with tickets_ours as (select t.passenger_name, t.ticket_no, tf.flight_id, f.scheduled_departure, tf.fare_conditions, tf.amount, row_number() over
-(partition by tf.flight_id order by t.ticket_no ) as posicion from flights f natural join ticket_flights tf natural join tickets t natural join bookings b where b.book_ref = '8E6BB3' and not exists (select 1 from boarding_passes bp where bp.flight_id =tf.flight_id AND bp.ticket_no = t.ticket_no) order by tf.ticket_no),
-	asientos_totales
-     AS (SELECT tou.flight_id,
+WITH booking_pasajeros
+     AS (SELECT t.passenger_name,
+                t.ticket_no,
+                tf.flight_id,
+                f.scheduled_departure,
+                tf.fare_conditions,
+                tf.amount,
+                Row_number()
+                  OVER (
+                    partition BY tf.flight_id
+                    ORDER BY t.ticket_no ) AS posicion
+         FROM   flights f
+                natural JOIN ticket_flights tf
+                natural JOIN tickets t
+                natural JOIN bookings b
+         WHERE  b.book_ref = '8E6BB3'
+                AND NOT EXISTS (SELECT '1'
+                                FROM   boarding_passes bp
+                                WHERE  bp.flight_id = tf.flight_id
+                                       AND bp.ticket_no = t.ticket_no)
+         ORDER  BY tf.ticket_no),
+     asientos_totales
+     AS (SELECT bps.flight_id,
                 s.seat_no AS asientos,
                 f.aircraft_code
          FROM   seats s,
                 aircrafts_data ad,
-                tickets_ours tou,
+                booking_pasajeros bps,
                 flights f
          WHERE  s.aircraft_code = ad.aircraft_code
                 AND ad.aircraft_code = f.aircraft_code
-                and f.flight_id = tou.flight_id
-         GROUP  BY tou.flight_id, s.seat_no, f.aircraft_code
-         order by aircraft_code, seat_no),
-    asientos_ocupados
-     AS (SELECT tou.flight_id,
+                AND f.flight_id = bps.flight_id
+         GROUP  BY bps.flight_id,
+                   s.seat_no,
+                   f.aircraft_code
+         ORDER  BY aircraft_code,
+                   seat_no),
+     asientos_ocupados
+     AS (SELECT bps.flight_id,
                 bp.seat_no AS asientos,
                 f.aircraft_code
-         FROM   tickets_ours tou,
+         FROM   booking_pasajeros bps,
                 ticket_flights tf,
                 boarding_passes bp,
                 flights f
-         WHERE  tou.flight_id = tf.flight_id
-         and tf.flight_id = bp.flight_id
-         and f.flight_id = tou.flight_id
-         GROUP  BY tou.flight_id, bp.seat_no, f,aircraft_code  order by bp.seat_no, f.aircraft_code),
-asientos_libres AS (
-    SELECT att.flight_id,
-           att.asientos AS vacios,
-          ROW_NUMBER() OVER (PARTITION BY att.flight_id ORDER BY att.flight_id, att.asientos) AS numero
- FROM asientos_totales att
- WHERE NOT EXISTS (
-   SELECT 1
-   FROM asientos_ocupados ao
-   WHERE ao.flight_id = att.flight_id AND ao.asientos = att.asientos))
-SELECT t.passenger_name, v.flight_id, f.scheduled_departure, av.vacios
-FROM asientos_libres av, tickets_ours v, tickets t, flights f
-where v.flight_id = av.flight_id and v.posicion = av.numero and t.ticket_no = v.ticket_no and f.flight_id = v.flight_id
+         WHERE  bps.flight_id = tf.flight_id
+                AND tf.flight_id = bp.flight_id
+                AND f.flight_id = bps.flight_id
+         GROUP  BY bps.flight_id,
+                   bp.seat_no,
+                   f,
+                   aircraft_code
+         ORDER  BY bp.seat_no,
+                   f.aircraft_code),
+     asientos_libres
+     AS (SELECT att.flight_id,
+                att.asientos                              AS vacios,
+                Row_number()
+                  OVER (
+                    partition BY att.flight_id
+                    ORDER BY att.flight_id, att.asientos) AS posicion
+         FROM   asientos_totales att
+         WHERE  NOT EXISTS (SELECT '1'
+                            FROM   asientos_ocupados ao
+                            WHERE  ao.flight_id = att.flight_id
+                                   AND ao.asientos = att.asientos))
+SELECT bps.passenger_name,
+       bps.flight_id,
+       bps.scheduled_departure,
+       av.vacios
+FROM   asientos_libres al,
+       booking_pasajeros bps
+WHERE  bps.flight_id = al.flight_id
+       AND bps.posicion = al.posicion
